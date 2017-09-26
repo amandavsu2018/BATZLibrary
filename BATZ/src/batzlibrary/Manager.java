d119576b13d99f2c52652ad883528b7ee272d8f8
Brandon Corbett <bmcorbet@valdosta.edu>
2017-09-26 15:25:00 -0400

    Added book table to db, createBook() to Manager Class

--- BATZ/src/batzlibrary/Manager.java
+++ BATZ/src/batzlibrary/Manager.java
@@ -1,5 +1,9 @@
 package batzlibrary;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Manager {
@@ -13,7 +17,8 @@ public class Manager {
 		int choice = 0;
 		System.out.println("What would you like to do?\n");
 		System.out.println("1: Add User to Database.");
-		System.out.println("2: Exit.");
+		System.out.println("2: Create book listing.");
+		System.out.println("3: Exit.");
 		Scanner choicescan = new Scanner(System.in);
 		// get the choice
 		while (true) {
@@ -23,7 +28,8 @@ public class Manager {
 					break;
 				} else if (choice == 2) {
 					break;
-				}
+				} else if (choice == 3)
+					break;
 			} catch (NumberFormatException nfe) {
 				System.out.print("Try again: ");
 			}
@@ -36,9 +42,60 @@ public class Manager {
 			cu.createUser();
 			break;
 		case 2:
+			createBook();
+		case 3:
 			break;
 		}
 		choicescan.close();
 		sessionOpen = false;
+	}
+
+	public void createBook(){
+		//variables
+		String bookTitle = null, bookAuthors = "", bookISBN = "", bookPubYear = "", bookKeywords = "", bookInvNum = "";
+		Database db = new Database();
+		String database = new String(db.getDatabase());
+		String databaseUser = new String(db.getDatabaseUser());
+		String databasePass = new String(db.getDatabasePassword());
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter book title.");
+		bookTitle = scan.nextLine().trim();
+		if (bookTitle != null) {
+			System.out.println("Enter book authors seperated by commas.");
+			bookAuthors = scan.nextLine().trim();
+			System.out.println("Enter book ISBN number.");
+			bookISBN = scan.nextLine().trim();
+			System.out.println("Enter book publication year.");
+			bookPubYear = scan.nextLine().trim();
+			System.out.println("Enter book keywords seperated by commas.");
+			bookKeywords = scan.nextLine().trim();
+			System.out.println("Enter number of books in inventory.");
+			bookInvNum = scan.nextLine().trim();
+		
+			try {
+				Connection conn = DriverManager.getConnection(database, databaseUser, databasePass);
+				System.out.println("Connection Object Created.\n");
+
+				String query = "INSERT INTO books (book_title, book_authors, book_ISBN, book_pubyear, book_keywords, book_invnum)"
+					+ "VALUES (?, ?, ?, ?, ?, ?);";
+				PreparedStatement preparedStmt = conn.prepareStatement(query);
+
+				preparedStmt.setString(1, bookTitle);
+				preparedStmt.setString(2, bookAuthors);
+				preparedStmt.setString(3, bookISBN);
+				preparedStmt.setString(4, bookPubYear);
+				preparedStmt.setString(5, bookKeywords);
+				preparedStmt.setString(6, bookInvNum);
+
+				System.out.println("Attempting database connection..\n");
+				preparedStmt.execute();
+				conn.close();
+				System.out.println("Updated database successfully.");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
