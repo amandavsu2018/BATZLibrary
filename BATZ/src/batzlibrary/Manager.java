38a2e96139baf30fa4b61a7f5d939f74dd28f03c
Brandon Corbett <bmcorbet@valdosta.edu>
2017-10-02 17:51:42 -0400

    Added editUser() to Manager class

--- BATZ/src/batzlibrary/Manager.java
+++ BATZ/src/batzlibrary/Manager.java
@@ -21,18 +21,24 @@ public class Manager {
 		System.out.println("What would you like to do?\n");
 		System.out.println("1: Add User to Database.");
 		System.out.println("2: Create book listing.");
-		System.out.println("3: Exit.");
+		System.out.println("3: Edit User Account.");
+		System.out.println("4: Exit.");
 		Scanner choicescan = new Scanner(System.in);
 		// get the choice
 		while (true) {
+			//these need to be here in order to set choice variable
+			//to a number 1-4 and nothing else
 			try {
 				choice = Integer.parseInt(choicescan.nextLine());
 				if (choice == 1) {
 					break;
 				} else if (choice == 2) {
 					break;
-				} else if (choice == 3)
+				} else if (choice == 3) {
 					break;
+				} else if (choice == 4) {
+					break;
+				}
 			} catch (NumberFormatException nfe) {
 				System.out.print("Try again: ");
 			}
@@ -47,10 +53,165 @@ public class Manager {
 		case 2:
 			createBook();
 		case 3:
+			editUser();
+		case 4:
 			break;
-		}
+		}	
 		choicescan.close();
 		sessionOpen = false;
+	}
+	
+	public void editUser(){
+		int choice = 0;
+		Database db = new Database();
+		String database = new String(db.getDatabase());
+		String databaseUser = new String(db.getDatabaseUser());
+		String databasePass = new String(db.getDatabasePassword());
+		String query = "";
+		
+		Scanner pinscan = new Scanner(System.in);
+		System.out.println("Please enter user's pin number.");
+		String pin = pinscan.next();
+		
+		//We need to add a checkPin() method later to check for existing pin
+		
+		System.out.println("What would you like to edit?\n");
+		System.out.println("1: Generate New Password.");
+		System.out.println("2: Set Locked to true or false.");
+		System.out.println("3: Edit Status.");
+		System.out.println("4: Edit First Name.");
+		System.out.println("5: Edit Last Name.");
+		System.out.println("6: Edit Street Address");
+		System.out.println("7: Edit City.");
+		System.out.println("8: Edit State.");
+		System.out.println("9: Edit Zip Code.");
+		System.out.println("10: Edit Phone.");
+		System.out.println("11: Exit.");
+		Scanner choicescan = new Scanner(System.in);
+		while (true) {
+			//these need to be here in order to set choice variable
+			//to a number 1-4 and nothing else
+			try {
+				choice = Integer.parseInt(choicescan.nextLine());
+				if (choice == 1) {
+					break;
+				} else if (choice == 2) {
+					break;
+				} else if (choice == 3) {
+					break;
+				} else if (choice == 4) {
+					break;
+				} else if (choice == 5) {
+					break;
+				} else if (choice == 6) {
+					break;
+				} else if (choice == 7) {
+					break;
+				} else if (choice == 8) {
+					break;
+				} else if (choice == 9) {
+					break;
+				} else if (choice == 10) {
+					break;
+				} else if (choice == 11) {
+					break;
+				}
+			} catch (NumberFormatException nfe) {
+				System.out.print("Try again: ");
+			}
+		}
+
+		// switch cases
+		switch (choice) {
+		case 1:
+			//generate password
+			String password = "";
+			CreateUser thing = new CreateUser();
+			password = thing.createPassword();
+			query = "UPDATE users SET user_password = '" + password + "'WHERE user_pin = '" + pin + "'";
+			System.out.println(password);
+			break;
+		case 2:
+			String locked = "";
+			Scanner l = new Scanner(System.in);
+			System.out.println("True or false?");
+			locked = l.nextLine();
+			query = "UPDATE users SET user_locked = '" + locked + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 3:
+			String status = "";
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter status: manager, associate, or member");
+			status = s.nextLine();
+			query = "UPDATE users SET user_status = '" + status + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 4:
+			String fName = "";
+			Scanner f = new Scanner(System.in);
+			System.out.println("First Name:");
+			fName = f.nextLine();
+			query = "UPDATE users SET user_firstname = '" + fName + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 5:
+			String lname = "";
+			Scanner ln = new Scanner(System.in);
+			System.out.println("Last Name:");
+			lname = ln.nextLine();
+			query = "UPDATE users SET user_lastname = '" + lname + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 6:
+			String street = "";
+			Scanner a = new Scanner(System.in);
+			System.out.println("Street:");
+			street = a.nextLine();
+			query = "UPDATE users SET user_street = '" + street + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 7:
+			String city = "";
+			Scanner c = new Scanner(System.in);
+			System.out.println("City:");
+			city = c.nextLine();
+			query = "UPDATE users SET user_city = '" + city + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 8:
+			String state = "";
+			Scanner st = new Scanner(System.in);
+			System.out.println("City:");
+			city = st.nextLine();
+			query = "UPDATE users SET user_state = '" + state + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 9:
+			String zip = "";
+			Scanner zi = new Scanner(System.in);
+			System.out.println("Zip:");
+			city = zi.nextLine();
+			query = "UPDATE users SET user_zip = '" + zip + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 10:
+			String phone = "";
+			Scanner ph = new Scanner(System.in);
+			System.out.println("Phone:");
+			city = ph.nextLine();
+			query = "UPDATE users SET user_phone = '" + phone + "'WHERE user_pin = '" + pin + "'";
+			break;
+		case 11:
+			break;
+		}
+		try{
+			Connection conn = DriverManager.getConnection(database, databaseUser, databasePass);
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("What would you like to do? 1. Edit more fields 2. Exit");
+		Scanner scan = new Scanner(System.in);
+		int thescan = scan.nextInt();
+		if(thescan == 1){
+			editUser();
+		} else {
+			sessionOpen = false;
+		}
 	}
 
 	public void createBook(){
