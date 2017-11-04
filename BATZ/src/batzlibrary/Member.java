6803d08ec3f8c4c92addfa4548b0fa9648e00393
Zack <zackeryweeks@gmail.com>
2017-11-03 21:44:11 -0400

    Created a CheckInBooks method

--- BATZ/src/batzlibrary/Member.java
+++ BATZ/src/batzlibrary/Member.java
@@ -13,7 +13,8 @@ public class Member {
 			int choice = 0;
 			System.out.println("What would you like to do?");
 			System.out.println("1: Search for books");
-			System.out.println("2: Exit.");
+			System.out.println("2: Return Books");
+			System.out.println("3: Exit.");
 			Scanner choicescan = new Scanner(System.in);
 			// get the choice
 			while (true) {
@@ -22,9 +23,11 @@ public class Member {
 					if (choice == 1) {
 						break;
 					} else if (choice == 2) {
+						break;
+					}else if (choice == 3) {
 						sessionOpen = false;
 						break;
-					} else {
+					}else {
 						sessionOpen = false;
 						break;
 					}
@@ -38,6 +41,10 @@ public class Member {
 			case 1:
 				SearchBooks sb = new SearchBooks();
 				sb.KeywordInput();
+				break;
+			case 2:
+				CheckInBook checkin = new CheckInBook();
+				checkin.putInDropBox();
 				break;
 			default:
 				sessionOpen = false;
